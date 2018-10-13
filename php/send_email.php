622c41d1a8b505362f8fc96068b4f4aed01947a7
Mohamed Sheik Dawood <mohamed@excelenciaconsulting.com>
2018-10-13 16:44:17 +0530

    Email id changed

--- php/send_email.php
+++ php/send_email.php
@@ -14,13 +14,13 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'misd.excelencia@gmail.com';                 // SMTP username
-    $mail->Password = 'excel.123';                           // SMTP password
+    $mail->Username = 'msramki1991@gmail.com';                 // SMTP username
+    $mail->Password = '******';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('misd.excelencia@gmail.com', 'Andaman Greenland');
+    $mail->setFrom('msramki1991@gmail.com', 'Andaman Greenland');
     $mail->addAddress($_POST['EmailId'], $_POST['CName']);     // Add a recipient
     //$mail->addAddress('ellen@example.com');               // Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
